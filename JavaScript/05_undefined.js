208aadfaad545148f7e56a45f75a7e5c1ed2a9c5
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-02-07 01:46:35 +0530

    js

---
+++ JavaScript/05_undefined.js
@@ -0,0 +1,17 @@
+
+// undefined and not defined difference
+
+console.log(a);
+var a;
+console.log(a);
+ if(a===undefined){
+    console.log("a is undefined")
+}
+else{
+    console.log("a is defined")
+}
+a = 10;
+console.log(a)
+
+//  not defined
+console.log(x)  
