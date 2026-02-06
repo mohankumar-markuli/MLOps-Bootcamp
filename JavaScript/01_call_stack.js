53986f0994963a31dbaef7554441567894890ff1
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-02-06 19:55:37 +0530

    update

---
+++ JavaScript/01_call_stack.js
@@ -0,0 +1,10 @@
+//  run html file on browser and pause debugger and check the execution scope 
+// Memory component and Code Component defference
+var x= 20
+function get_message(){
+    return "Hello JavaScript"
+}
+
+get_message()
+console.log(get_message())
+console.log(x)
