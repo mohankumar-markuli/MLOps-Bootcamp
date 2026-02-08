b4217a1e7cbf596be48ac1849a48b8d8933d5885
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-02-08 21:14:05 +0530

    map

--- JavaScript/17_map.js
+++ JavaScript/17_map.js
@@ -9,11 +9,23 @@ const output = arr.map(double)
 const output2 = arr.map(function double(x){
     return x*2;
 });
+
+
 const output3 = arr.map((x)=>  x*2)
 
 console.log(output)
 console.log(output2)
 console.log(output3)
 
-console.log(filter_output2)
+// use of reduce in place of map
+reduced_arr = []
+const reduce_output = arr.reduce(function abc(acc,curr){
+    acc = curr *2
+    reduced_arr.push(acc)
+    return acc
+},reduced_arr)
+
+console.log('reduce result',reduced_arr)
+
+
 
